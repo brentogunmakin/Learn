9bb2a6b17854ca0b690b8422451e4f8f24e8833d
brentogunmakin <brent.ogunmakin@us.af.mil>
2020-11-19 10:47:49 -0700

    Finished 6ees6ees6ees

---
+++ 6ees6ees6ees/src/ForagerBee.js
@@ -0,0 +1,15 @@
+class ForagerBee extends Bee{
+    constructor (){
+        super();
+        this.age = 10;
+        this.job = 'find pollen';
+        this.canFly = true;
+        this.treasureChest = [];
+
+
+    }
+    forage(item) {
+        this.treasureChest.push(item);
+
+    }
+}
